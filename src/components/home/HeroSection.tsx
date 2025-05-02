ac5f618f8a9668fdd9fb6a197304bbfb5a2158a5
vincent bachelet <vincent.bachelet@needhelp.com>
2025-05-02 14:14:08 +0200

    feat: changing favicone

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -21,7 +21,7 @@ const HeroSection = () => {
 
       <div className="absolute left-0 right-0 bottom-[20%] md:bottom-[10%] px-4 w-full">
         <div className="container mx-auto">
-             <div className="flex justify-center">
+          <div className="flex justify-center ">
             <div className="flex flex-col md:flex-row gap-4 items-center max-w-2xl mx-auto px-4 py-6 bg-white/70 backdrop-blur-md rounded-2xl shadow-lg border border-white/40">
               <Link 
                 to={links.internal.tickets} 
