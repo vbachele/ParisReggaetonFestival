411aaeb52068054cad0b439fea459bbf9f01a26a
vincent bachelet <vincent.bachelet@needhelp.com>
2025-05-02 14:07:04 +0200

    feat: pansements button homepage

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -19,21 +19,24 @@ const HeroSection = () => {
         />
       </div>
 
-      <div className="absolute left-0 right-0 bottom-[24%] md:bottom-[12%] px-4 w-full">
+      <div className="absolute left-0 right-0 bottom-[20%] md:bottom-[10%] px-4 w-full">
         <div className="container mx-auto">
-          <div className="flex flex-col md:flex-row md:gap-4 gap-2 justify-center items-center max-w-2xl mx-auto">
-            <Link 
-              to={links.internal.tickets} 
-              className="w-full md:w-auto bg-text-primary text-white text-xl md:text-2xl font-bold px-10 py-3 md:py-5 rounded-2xl transition-all inline-flex items-center justify-center gap-3 transform hover:scale-105"
-            >
-              BILLETTERIE
-            </Link>
-            <Link 
-              to={links.internal.planning} 
-              className="w-full md:w-auto bg-white text-black text-xl md:text-2xl font-bold px-10 py-3 md:py-5 rounded-2xl transition-all inline-flex items-center justify-center gap-3 transform hover:scale-105"
-            >
-              PLANNING
-            </Link>
+             <div className="flex justify-center">
+            <div className="flex flex-col md:flex-row gap-4 items-center max-w-2xl mx-auto px-4 py-6 bg-white/70 backdrop-blur-md rounded-2xl shadow-lg border border-white/40">
+              <Link 
+                to={links.internal.tickets} 
+                className="w-full md:w-auto bg-text-primary text-white text-xl md:text-2xl font-bold px-10 py-3 md:py-5 rounded-2xl transition-all inline-flex items-center justify-center gap-3 transform hover:scale-105"
+              >
+                BILLETTERIE
+              </Link>
+              <Link 
+                to={links.internal.planning} 
+                className="w-full md:w-auto bg-white text-black text-xl md:text-2xl font-bold px-10 py-3 md:py-5 rounded-2xl transition-all inline-flex items-center justify-center gap-3 transform hover:scale-105"
+              >
+                PLANNING
+              </Link>
+            </div>
+         
           </div>
         </div>
       </div>
